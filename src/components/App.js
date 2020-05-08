d4f7405b1bc22b4cad5d761db6a439c2b9082166
Adrian Ramos <ramosadrian56@gmail.com>
2020-05-07 19:23:29 -0700

    add initial components

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import UserCreate from './UserCreate';
+
+export class App extends Component {
+    state = {
+        language: 'english'
+    }
+
+    onLanguageChange = (language) => {
+        this.setState({ language })
+    };
+
+    render() {
+        return (
+            <div className="ui container">
+                <div>
+                    Select a language: 
+                    <i className="flag us" onClick={() => this.onLanguageChange('english')}/>
+                    <i className="flag nl" onClick={() => this.onLanguageChange('dutch')}/>
+                    <i className="flag mx" onClick={() => this.onLanguageChange('spanish')}/>
+                </div>
+                <UserCreate />
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ src/components/Field.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Feild extends Component {
+    render() {
+        return (
+            <div className="ui field">
+                <label>Name</label>
+                <input />
+            </div>
+        );
+    };
+};
+
+export default Feild;
